163206d29abd5d3a8a9748924c5f61fb29e2ae80
Matan Lurey <matan@lurey.org>
2023-07-30 16:08:32 -0700

    Actually remove.

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:sticky_headers/sticky_headers.dart';
 
 import '/src/models.dart' hide Card;
 import '/src/themes.dart';
